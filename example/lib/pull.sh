6d16dcd05961ba0999b7a171742ba5183831245a
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2025-01-22 11:24:30 +0700

    chore: script to pull example dependency

---
+++ example/lib/pull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone --depth=1 --branch=v1.91.6-docking https://github.com/ocornut/imgui.git imgui-docking
+git clone --depth=1 --branch=release-2.30.3 https://github.com/libsdl-org/SDL.git sdl2
+git clone --depth=1 --branch=v2.0.6 https://github.com/Dav1dde/glad.git glad2
